37892862d47d7eee0505eb095aee6e09831199b3
oceansalle <oceanesalleiae@gmail.com>
2023-03-09 09:52:10 +0100

    calendar

--- src/App.js
+++ src/App.js
@@ -9,11 +9,12 @@ import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
 //import Bar from "./scenes/bar";
 import Form from "./scenes/form";
+import Calendar from "./scenes/calendar/calendar.jsx";
 //import Line from "./scenes/line";
 //import Pie from "./scenes/pie";
 //import FAQ from "./scenes/faq";
 //import Geography from "./scenes/geography";
-//import Calendar from "./scenes/calendar/calendar";
+
 
 function App() {
   const [theme, colorMode] = useMode(); 
@@ -32,11 +33,12 @@ function App() {
             <Route path="/contacts" element={<Contacts />} />  
             <Route path="/invoices" element={<Invoices />} /> 
             <Route path="/form" element={<Form />} /> 
+            <Route path="/calendar" element={<Calendar />} /> 
              {/*  <Route path="/bar" element={<Bar />} />  */}
              {/*  <Route path="/pie" element={<Pie />} />  */}
              {/*  <Route path="/line" element={<Line />} />  */}
              {/*  <Route path="/faq" element={<FAQ />} />  */}
-             {/*  <Route path="/calendar" element={<Calendar />} />  */}
+             
              {/* <Route path="/geography" element={<Geography />} />  */}
             </Routes>
       </main>
