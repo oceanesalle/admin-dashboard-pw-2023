8187b0a9963b62d77de1c94e2b12ac24c05aea37
oceansalle <oceanesalleiae@gmail.com>
2023-03-09 10:01:29 +0100

    faq page

--- src/App.js
+++ src/App.js
@@ -9,10 +9,11 @@ import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
 //import Bar from "./scenes/bar";
 import Form from "./scenes/form";
-import Calendar from "./scenes/calendar/calendar.jsx";
+//import Calendar from "./scenes/calendar/calendar.jsx";
+import FAQ from "./scenes/faq";
 //import Line from "./scenes/line";
 //import Pie from "./scenes/pie";
-//import FAQ from "./scenes/faq";
+
 //import Geography from "./scenes/geography";
 
 
@@ -33,11 +34,12 @@ function App() {
             <Route path="/contacts" element={<Contacts />} />  
             <Route path="/invoices" element={<Invoices />} /> 
             <Route path="/form" element={<Form />} /> 
-            <Route path="/calendar" element={<Calendar />} /> 
+             {/*<Route path="/calendar" element={<Calendar />} /> */}
+            <Route path="/faq" element={<FAQ />} /> 
              {/*  <Route path="/bar" element={<Bar />} />  */}
              {/*  <Route path="/pie" element={<Pie />} />  */}
              {/*  <Route path="/line" element={<Line />} />  */}
-             {/*  <Route path="/faq" element={<FAQ />} />  */}
+             
              
              {/* <Route path="/geography" element={<Geography />} />  */}
             </Routes>
