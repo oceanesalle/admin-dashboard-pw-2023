14047c7bf6623b1b0cd665eb058b610990116860
oceansalle <oceanesalleiae@gmail.com>
2023-03-10 12:51:16 +0100

    geography chart

--- src/App.js
+++ src/App.js
@@ -13,8 +13,7 @@ import Form from "./scenes/form";
 import FAQ from "./scenes/faq";
 import Line from "./scenes/line";
 import Pie from "./scenes/pie";
-
-//import Geography from "./scenes/geography";
+import Geography from "./scenes/geography";
 
 
 function App() {
@@ -39,9 +38,7 @@ function App() {
             <Route path="/bar" element={<Bar />} /> 
             <Route path="/pie" element={<Pie />} />  
             <Route path="/line" element={<Line />} />
-             
-             
-             {/* <Route path="/geography" element={<Geography />} />  */}
+            <Route path="/geography" element={<Geography />} /> 
             </Routes>
       </main>
       </div>;
